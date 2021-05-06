349f9c0a767495c986a6588b01f6b3af7f4c8cd6
joy210 <sohanoor404@gmail.com>
2021-05-07 03:07:32 +0600

    update navbar

--- src/components/partials/Nav.js
+++ src/components/partials/Nav.js
@@ -1,5 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useHistory, Link } from "react-router-dom";
+import { selectUser } from "../../features/userSlice";
+import { auth } from "../../firebase";
 import "./Nav.css";
 
 const logo = "https://bit.ly/3aBYy1P";
@@ -7,6 +11,8 @@ const avatar = "https://bit.ly/2QS8b5M";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const history = useHistory();
+  const user = useSelector(selectUser);
 
   const transitionNavbar = () => {
     if (window.scrollY > 100) {
@@ -23,13 +29,33 @@ function Nav() {
   }, []);
 
   return (
-    <nav className={`navbar fixed-top ${show && "navbar-dark"}`}>
-      <a className="nav-logo" href="#">
+    <nav
+      className={`navbar fixed-top navbar-expand-lg ${show && "navbar-dark"}`}
+    >
+      <a className="nav-logo" href="" onClick={() => history.push("/")}>
         <img src={logo} alt="" />
       </a>
-      <a className="nav-avatar ml-auto" href="#">
-        <img src={avatar} alt="" />
-      </a>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <Link to="/" className="nav-link text-white  ">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item active">
+          <Link to="/profile" className="nav-link text-white ">
+            Profile
+          </Link>
+        </li>
+        <li className="nav-item active">
+          <a
+            className="nav-link text-white  nav-avatar"
+            href=""
+            onClick={() => auth.signOut()}
+          >
+            Logout
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
